21bf5af5dd7a66ab17801621b2929255535a783c
JulioVS <julioer@me.com>
2026-01-16 19:40:28 -0300

    Add URLSession object and task call

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -12,11 +12,47 @@ struct WeatherManager {
     
     // I avoid hard-coding API keys when uploading projects to GitHub!
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=_PASTE_MY_ACTUAL_API_KEY_HERE_&units=metric"
-    
+
     func fetchWeather(cityName: String) {
         
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+        
+    }
+    
+    func performRequest(urlString: String) {
+        
+        // 1. Create a URL
+        if let url = URL(string: urlString) {
+            
+            // 2. Create a URL Session
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            // 4. Start the task
+            task.resume()
+            
+        }
+        
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) -> Void {
+        
+        if error != nil {
+            
+            print(error!)
+            return
+            
+        }
+        
+        if let safeData = data {
+            
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!)
+            
+        }
         
     }
     
