9400acd43c595a4b4e24b64c8ccc529f067d7cf7
JulioVS <julioer@me.com>
2026-01-17 16:55:59 -0300

    Add closure and JSON parsing

---
+++ Clima/WeatherData.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Julio Errecart on 17/1/26.
+//  Copyright © 2026 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    
+}
+
+struct Main: Decodable {
+    
+    let temp: Double
+    
+}
+
+struct Weather: Decodable {
+    
+    let description: String
+    
+}
--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -12,7 +12,7 @@ struct WeatherManager {
     
     // I avoid hard-coding API keys when uploading projects to GitHub!
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=_PASTE_MY_ACTUAL_API_KEY_HERE_&units=metric"
-
+    
     func fetchWeather(cityName: String) {
         
         let urlString = "\(weatherURL)&q=\(cityName)"
@@ -22,35 +22,49 @@ struct WeatherManager {
     
     func performRequest(urlString: String) {
         
-        // 1. Create a URL
         if let url = URL(string: urlString) {
             
-            // 2. Create a URL Session
             let session = URLSession(configuration: .default)
             
-            // 3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                
+                if error != nil {
+                    
+                    print(error!)
+                    return
+                    
+                }
+                
+                if let safeData = data {
+                    
+                    parseJSON(weatherData: safeData)
+                    
+                }
+                
+            }
             
-            // 4. Start the task
             task.resume()
             
         }
         
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) -> Void {
+    func parseJSON(weatherData: Data) {
         
-        if error != nil {
+        let decoder = JSONDecoder()
+        
+        do {
             
-            print(error!)
-            return
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            
+            print(decodedData.name)
+            print(decodedData.main.temp)
+            print(decodedData.weather[0].description)
             
         }
-        
-        if let safeData = data {
+        catch {
             
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString!)
+            print(error)
             
         }
         
